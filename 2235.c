68ee0d612310d916b6896a2da5ac1d062f224c04
Mateus Freitas <mateus_freitascorreia@hotmail.com>
2021-04-25 01:11:25 -0300

    Add Files

--- 2235.c
+++ 2235.c
@@ -6,35 +6,6 @@
 #include <stdio.h>
  
 int main() {
-    unsigned long long x, resultado = 0;
-    int n, i, j;
-
-    scanf("%d", &n);
-    for(i = 1; i <= n; i++){
-        resultado = 0;
-        scanf("%llu", &x);
-
-        for(j = 1; j < sqrt(x); j++){
-            if (x%j == 0){
-                resultado = resultado + 1;
-            }if(resultado > 1){
-                break;
-            }
-        }
-        if (resultado == 1){
-            printf("Prime\n");
-        }else {
-            printf("Not Prime\n");
-        }
-
-    }
-
-return 0;
-}
-
-#include <stdio.h>
- 
-int main() {
     int a, b, c;
 
     scanf("%d %d %d",&a,&b,&c);
