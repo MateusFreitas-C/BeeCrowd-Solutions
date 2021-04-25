152ba014d2f4f329ef34d93fe86e90abc62be12a
Mateus Freitas <mateus_freitascorreia@hotmail.com>
2021-04-25 01:10:38 -0300

    Add Files

--- 2235.c
+++ 2235.c
@@ -1,3 +1,37 @@
+/*
+  Autor: Mateus Freitas;
+  Nome: Andando no Tempo;
+*/
+
+#include <stdio.h>
+ 
+int main() {
+    unsigned long long x, resultado = 0;
+    int n, i, j;
+
+    scanf("%d", &n);
+    for(i = 1; i <= n; i++){
+        resultado = 0;
+        scanf("%llu", &x);
+
+        for(j = 1; j < sqrt(x); j++){
+            if (x%j == 0){
+                resultado = resultado + 1;
+            }if(resultado > 1){
+                break;
+            }
+        }
+        if (resultado == 1){
+            printf("Prime\n");
+        }else {
+            printf("Not Prime\n");
+        }
+
+    }
+
+return 0;
+}
+
 #include <stdio.h>
  
 int main() {
