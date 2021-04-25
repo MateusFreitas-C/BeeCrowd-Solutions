526876ea726912dc562d6bac28644f29fe7c6bcf
Mateus Freitas <mateus_freitascorreia@hotmail.com>
2021-04-25 00:43:28 -0300

    q

---
+++ 1101.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int main(void){
+    int m, n, soma, i, x;
+
+    soma = 0;
+    x = 0;
+
+    do{
+        scanf("%d%d", &m, &n);
+        if (m > 0 && n > 0){
+             if(m < n){
+                for(i = m; i <= n; i++){
+                    soma = soma + i;
+                    printf("%d ", i);
+                }
+                printf("Sum=%d\n", soma);
+                soma = 0;
+            }else{
+                for(i = n; i <= m; i++){
+                    soma = soma + i;
+                    printf("%d ", i);
+                }
+                printf("Sum=%d\n", soma);
+                soma = 0;
+            }
+        }else {
+            return 0;
+    }
+    }
+    while(m > 0 || n > 0);
+
+	return 0;
+}
