3cf31c6fddd626407749628a6f7d414389818bd8
Mateus Freitas <mateus_freitascorreia@hotmail.com>
2021-04-25 00:58:38 -0300

    Add Files

---
+++ 1805.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+ 
+int main() {
+    unsigned long long m, n, soma;
+
+    scanf("%llu%llu", &m, &n);
+    soma = ((m + n) * (n - m + 1)) / 2;
+    printf("%llu\n", soma);
+
+return 0;
+}
