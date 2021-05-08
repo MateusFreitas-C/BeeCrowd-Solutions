90e49c6e9a0fbf5c7dbeb449f28fba15b37f23a3
Mateus Freitas <mateus_freitascorreia@hotmail.com>
2021-05-08 20:15:24 -0300

    Add Files

---
+++ 1028.py
@@ -0,0 +1,13 @@
+def pilha(F1, F2):
+    if F1 % F2 == 0:
+        return F2
+    else:
+        return pilha(F2, F1 % F2)
+
+
+testes = int(input())
+for c in range(testes):
+    a = input().split(" ")
+    F1 = int(a[0])
+    F2 = int(a[1])
+    print(pilha(F1, F2))
